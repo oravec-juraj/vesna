d570a08d2983dcee19782fa1119ac6d796eec1e0
xhoreckym <125309188+xhoreckym@users.noreply.github.com>
2024-03-11 14:29:48 +0100

    Add files via upload
    Humidity and temp sensor

---
+++ MeteoTeam2024/FS400_SHT31.ino
@@ -0,0 +1,42 @@
+#include "Adafruit_SHT31.h" //https://github.com/adafruit/Adafruit_SHT31/archive/master.zip
+Adafruit_SHT31 sht31 = Adafruit_SHT31();
+
+#include <Wire.h>
+
+void setup() {
+      Serial.begin(9600);
+      delay(10);
+      Serial.println("SHT31 test");
+      if (! sht31.begin(0x44)) {   // Set to 0x45 for alternate i2c addr
+      Serial.println("Couldn't find SHT31");
+      while (1) delay(1);
+      }
+
+
+  }
+
+
+void loop() {
+  
+    float t = sht31.readTemperature();
+    float h = sht31.readHumidity();
+    
+    if (! isnan(t)) {  // check if 'is not a number'
+    Serial.print("Temp *C = "); Serial.println(t);
+    }
+    else {    
+    t=0.0;
+    Serial.println("Failed to read temperature");
+    }
+    
+    if (! isnan(h)) {  // check if 'is not a number'
+    Serial.print("Hum. % = "); Serial.println(h);
+    }
+    else { 
+    h=0.0;
+    Serial.println("Failed to read humidity");    
+    }
+    Serial.println();
+    delay(500);
+
+}
