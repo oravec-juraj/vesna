1435dfb696b1f77455a69509313e7a3b3e70a935
Ivana Dukayová <125315008+heviga@users.noreply.github.com>
2024-03-18 13:42:19 +0100

    Update all_sensors_try.ino

--- MeteoTeam2024/all_sensors_try.ino
+++ MeteoTeam2024/all_sensors_try.ino
@@ -3,18 +3,17 @@
 #include <Adafruit_LPS35HW.h>
 #include "Adafruit_SHT4x.h"
 
-Adafruit_SHT31 sht30 = Adafruit_SHT31();
+Adafruit_SHT31 sht30 = Adafruit_SHT31();//sht30
 Adafruit_LPS35HW lps35hw = Adafruit_LPS35HW();
 Adafruit_SHT4x sht4 = Adafruit_SHT4x();
+Adafruit_SHT31 sht31 = Adafruit_SHT31();//shtxx
+
 
 void setup() {
     Serial.begin(9600);
     delay(10);
 
-    if (!sht30.begin(0x44)) {
-        Serial.println("Couldn't find SHT30");
-        while (1) delay(1);
-    }
+
 
     if (!lps35hw.begin_I2C()) {
         Serial.println("Couldn't find LPSHW35");
@@ -25,6 +24,15 @@ void setup() {
         Serial.println("Couldn't find SHT4x");
         while (1) delay(1);
     }
+
+     if (! sht31.begin(0x45)) {   // Set to 0x45 for alternate i2c addr
+      Serial.println("Couldn't find SHT31");
+      while (1) delay(1);
+      }
+    if (!sht30.begin(0x44)) {
+            Serial.println("Couldn't find SHT30");
+            while (1) delay(1);
+        }
 
     Serial.print("Serial number 0x");
     Serial.println(sht4.readSerial(), HEX);
@@ -38,6 +46,9 @@ void loop() {
 
     float tLPS35hw = lps35hw.readTemperature();
     float pressureLPS35hw = lps35hw.readPressure();
+
+    float tfs400 = sht31.readTemperature();
+    float hfs400 = sht31.readHumidity();
 
     sensors_event_t hlps45, tlps45; // Used for both temp and humidity
     tlps45.temperature = 0.0; // Initialize the temperature variable
@@ -93,6 +104,23 @@ void loop() {
     } else {
         Serial.println("Failed to read humidity from SHT45");
     }
+
+    if (! isnan(tfs400)) {  // check if 'is not a number'
+    Serial.print("Temp *C = "); Serial.println(tfs400);
+    }
+    else {    
+    tfs400=0.0;
+    Serial.println("Failed to read temperature");
+    }
+    
+    if (! isnan(hfs400)) {  // check if 'is not a number'
+    Serial.print("Hum. % = "); Serial.println(hfs400);
+    }
+    else { 
+    hfs400=0.0;
+    Serial.println("Failed to read humidity");    
+    }
+
 
     Serial.println();
     delay(500);
